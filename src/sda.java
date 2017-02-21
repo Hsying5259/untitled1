2cfacd9dd29fa7411ccc50b3d94affa2f703fae7
Hsying5259 <1003178097@qq.com>
2017-02-21 09:35:31 +0800

    sfawd

--- src/sda.java
+++ src/sda.java
@@ -2,5 +2,7 @@
  * Created by Administrator on 2017/2/21.
  */
 public class sda {
-    public static void main()
+    public static void main(String[] args){
+        System.out.println("这是我的第一个Java程序");
+    }
 }
